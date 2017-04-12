a164839946409b2e861e326c97c69def0523be7c
edison <71226088@qq.com>
2017-04-12 16:37:32 +0800

    by edison

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 #import tensorflow as tf
 #print(type(tf))
 
+
 print(mnist.train.images.shape, mnist.train.labels.shape)
 print(mnist.test.images.shape, mnist.test.labels.shape)
 print(mnist.validation.images.shape, mnist.validation.labels.shape)
